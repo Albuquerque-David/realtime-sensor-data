f52a25e22936f861296df56d4907882100f6cce6
David A <38769003+Albuquerque-David@users.noreply.github.com>
2024-12-06 21:55:42 -0300

    fix: env

--- backend/config/database.py
+++ backend/config/database.py
@@ -7,7 +7,7 @@ logger = get_logger("RealtimeSensorDataAPI")
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/sensor_data")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/sensor_data")
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["sensor_data"]
 
